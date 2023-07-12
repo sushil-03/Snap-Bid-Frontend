217db3e7a656ddbb902ce1e3ff6ac5cfde3579f0
Sushil <sushilrawat1720@gmail.com>
2023-07-12 12:50:54 +0530

    update ui

--- src/endpoints/axios.ts
+++ src/endpoints/axios.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/",
-  //   baseURL: "https://snapbid-backend.onrender.com",
+  // baseURL: "http://localhost:3001/",
+  baseURL: "https://snapbid-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
